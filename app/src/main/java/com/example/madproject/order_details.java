2c52df58d979cdb4aa706853d5fa0abb01c5018b
Sayuri <it20197032@my.sliit.lk>
2021-09-23 00:46:07 +0530

    sayuri ui parts

---
+++ app/src/main/java/com/example/madproject/order_details.java
@@ -0,0 +1,16 @@
+package com.example.madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.madproject.R;
+
+public class order_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details);
+    }
+}
