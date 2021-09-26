f3641cd70831ffba66feafe4597c23dd72fd68bf
Thisara Chandrawansa <75584732+thisara20@users.noreply.github.com>
2021-09-27 00:51:50 +0530

    request newbook,feedback,user profile,string,colors,image1 folder to drawable added

---
+++ app/src/main/java/com/example/madproject/MainActivity_login.java
@@ -0,0 +1,18 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class MainActivity_login extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login);
+    }
+}
---
+++ app/src/main/java/com/example/madproject/Member.java
@@ -0,0 +1,36 @@
+package com.example.login;
+
+public class Member {
+    private String name;
+    private String password;
+    private String email;
+
+    public Member() {
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
