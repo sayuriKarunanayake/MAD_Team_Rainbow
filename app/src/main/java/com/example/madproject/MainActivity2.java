81f415c8c83c8dd7be9fa12a2fa3163a8ca85b06
thisara20 <chandrawanshathisara@gmail.com>
2022-09-28 11:10:42 +0530

    added java files

---
+++ app/src/main/java/com/example/madproject/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RatingBar;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/madproject/act2_signup.java
@@ -0,0 +1,14 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class act2_signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act2_signup);
+    }
+}
